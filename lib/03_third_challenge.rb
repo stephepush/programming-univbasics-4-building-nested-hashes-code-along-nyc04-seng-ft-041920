1faab9d44544f6a56c967e3dc5879c5af79a3c11
Stephen Peters <7speter@gmail.com>
2020-04-30 15:19:49 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -14,8 +14,7 @@ def third_challenge
         name: "Romeo", age: "15", status: "alive"
         },
       :hero_friends => [
-        {name: "Benvolio", age: "17", attitude: "worried"},
-        {name: "Mercutio", age: "18", attitude: "hot-headed"}
+        
       ]
    },
    :capulet => {
@@ -32,8 +31,7 @@ def third_challenge
         name: "Juliet", age: "15", status: "alive"
         },
       :heroine_friends => [
-        {name: "Steven", age: "30", attitude: "confused"},
-        {name: "Nurse", age: "44", attitude: "worried"}
+        
       ]
    }
   }
